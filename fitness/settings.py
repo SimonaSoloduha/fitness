ce05ed45057635497738e76ca2cd2776e6c6d862
Simona Soloduha <ls460simona@gmail.com>
2023-03-30 18:02:22 +0300

    add settings fix

--- fitness/settings.py
+++ fitness/settings.py
@@ -152,10 +152,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = '/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = 'media'
+MEDIA_ROOT = '/media/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
