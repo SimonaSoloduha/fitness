be5b072506c5772fc4943e76249646f5668c93b4
Simona Soloduha <ls460simona@gmail.com>
2023-08-13 20:12:44 +0300

    fix amvera

--- fitness/celery.py
+++ fitness/celery.py
@@ -1,11 +1,26 @@
+# import os
+# from celery import Celery
+# from django.conf import settings
+#
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fitness.settings')
+#
+# app = Celery('fitness')
+#
+# app.config_from_object('django.conf:settings')
+# app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 import os
 from celery import Celery
 from django.conf import settings
-
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fitness.settings')
 
 app = Celery('fitness')
 
-app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+try:
+    app.config_from_object('django.conf:settings')
+    app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+except Exception as e:
+    print(f"\n\n\n546275482746568724568 An error occurred: {e}")
+    raise
+
