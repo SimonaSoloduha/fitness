1e597e4a0d8d13b520a99b676d3a1cb2e1b87966
Simona Soloduha <ls460simona@gmail.com>
2023-08-17 12:49:52 +0300

    fix

--- fitness/settings.py
+++ fitness/settings.py
@@ -95,23 +95,23 @@ WSGI_APPLICATION = 'fitness.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': env('DATABASE_NAME', default=''),
-#         'USER': env('DATABASE_USER_NAME', default=''),
-#         'PASSWORD': env('DATABASE_USER_PASSWORD', default=''),
-#         'HOST': env('DATABASE_HOST', default=''),
-#         'PORT': '',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('NAME', default=''),
+        'USER': env('USER', default=''),
+        'PASSWORD': env('PASSWORD', default=''),
+        'HOST': env('DATABASE_HOST', default=''),
+        'PORT': '',
+    }
+}
 
 # DATABASES = {
 #     'default': {
