e8815cac3517a85521f86f0e18d0cb8df0df856a
simonasoloduha <ls460simona@gmail.com>
2024-05-06 22:04:19 +0300

    add correct for sprinthost

--- fitness/settings.py
+++ fitness/settings.py
@@ -98,16 +98,27 @@ WSGI_APPLICATION = 'fitness.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': env('NAME', default=''),
+#         'USER': env('USER', default=''),
+#         'PASSWORD': env('PASSWORD', default=''),
+#         'HOST': '127.0.0.1',
+#         'PORT': 5432,
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': env('NAME', default=''),
         'USER': env('USER', default=''),
         'PASSWORD': env('PASSWORD', default=''),
-        'HOST': '127.0.0.1',
-        'PORT': 5432,
+        'HOST': 'localhost',
     }
 }
+
 # DATABASES = {
 #     'default': {
 #         # If you are using Cloud SQL for MySQL rather than PostgreSQL, set
