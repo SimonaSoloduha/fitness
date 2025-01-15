8dea66726654ff20780823e3b99167ee1e078f76
simonasoloduha <ls460simona@gmail.com>
2025-01-15 17:48:38 +0300

    fix style

--- subscription/urls.py
+++ subscription/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from subscription.views import subscription, subscription_fit_vid_promocode
+from subscription.views import subscription, subscription_fit_vid_promocode, payment_subscription_fit_vid
 
 urlpatterns = [
     path('subscription/', subscription, name='subscription'),
-    path('subscription/subscription_fit_vid_promocode', subscription_fit_vid_promocode, name='subscription_fit_vid_promocode')
+    path('subscription/subscription_fit_vid_promocode', subscription_fit_vid_promocode,
+         name='subscription_fit_vid_promocode'),
+    path('subscription/payment_subscription_fit_vid', payment_subscription_fit_vid,
+         name='payment_subscription_fit_vid')
 
 ]
--- subscription/views.py
+++ subscription/views.py
@@ -1,9 +1,9 @@
 from django.http import JsonResponse
 from django.utils.translation import gettext_lazy as _
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
-from django.contrib.auth.models import User
-
+from authentication.forms import RegisterForm
 from subscription.forms import SubscriptionFitnessVideoForm
 from subscription.models import Subscription, SubscriptionFitnessVideo, PromoCodeFitnessVideo
 from subscription.tasks import send_hello_to_email
@@ -42,13 +42,10 @@ def subscription_fit_vid_promocode(request):
         promo_code = request.POST.get('promo_code')
         user = request.user
         try:
-            # subscription_fit_vid = SubscriptionFitnessVideo.objects.get(user=user)
             promo_code_fit_vid = PromoCodeFitnessVideo.objects.get(code=promo_code)
-            print(promo_code, 'promo_code_fit_vid', promo_code_fit_vid.code)
             user_subscription_fit_vid = (SubscriptionFitnessVideo.objects.
                                          filter(user=user, sub_type__contains=promo_code_fit_vid.sub_type, active=True).
                                          first())
-            print(user_subscription_fit_vid, 'user_subscription_fit_vid')
 
             if user_subscription_fit_vid:
                 attention = f'У вас активна такая подписка до {user_subscription_fit_vid.data_finish.strftime("%Y-%m-%d %H:%M:%S")}'
@@ -62,16 +59,8 @@ def subscription_fit_vid_promocode(request):
                     sub_type=promo_code_fit_vid.sub_type,
                 )
                 user_subscription_fit_vid.save()
+                attention = f'Ваша подписка оформлена'
 
-                # send_hello_to_email(email)
-                message = _('Ваша подписка оформлена')
-                attention = f'Cрвпароыфвалп'
-
-        # except SubscriptionFitnessVideo.DoesNotExist:
-        #     new = SubscriptionFitnessVideo(user=user)
-        #     new.save()
-        #     # send_hello_to_email(email)
-        #     message = _('Ваша подписка оформлена')
         except PromoCodeFitnessVideo.DoesNotExist:
             attention = f'Такого промокода нет 🤪'
         context = {
@@ -80,3 +69,25 @@ def subscription_fit_vid_promocode(request):
         }
 
     return render(request, 'subscription/subscription_fit_vid_promocode.html', context)
+
+
+def payment_subscription_fit_vid(request):
+    """
+    Представление покупки подсписки на график
+    """
+
+    if request.method == "POST":
+        form_register = RegisterForm(request.POST)
+        form_subscription_fitness_video = SubscriptionFitnessVideoForm()
+
+        if form_register.is_valid():
+            user = form_register.save()
+            user.save()
+            raw_password = form_register.cleaned_data.get('password1')
+            email = form_register.cleaned_data.get('email')
+            user = authenticate(username=user.username, email=email, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form_register = RegisterForm()
+    return render(request, 'subscription/pay_subscription.html', {'form': form_register})
