ebb007e1e1e2755b9ab446e27aaf32bc343c59b4
simonasoloduha <ls460simona@gmail.com>
2025-01-23 15:53:05 +0300

    fix path dogovor

--- dogovor/views.py
+++ dogovor/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import FileResponse
-
-# Create your views here.
+import os
 
 
 def dogovor(request):
-    filepath = 'static/files/dogovor.pdf'
+    dogovor_path = 'static/files/dogovor.pdf'
+    basedir = os.path.abspath(os.getcwd())
+    filepath = os.path.abspath(os.path.join(basedir, dogovor_path))
+
     return FileResponse(open(filepath, 'rb'), content_type='application/pdf')
