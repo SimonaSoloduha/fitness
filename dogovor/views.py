6be840880b048f3922b5da270588d91f2091de76
simonasoloduha <ls460simona@gmail.com>
2025-01-23 16:41:18 +0300

    fix path dogovor

--- dogovor/views.py
+++ dogovor/views.py
@@ -4,15 +4,6 @@ import os
 
 
 def dogovor(request):
-    dogovor_path = '/static/files/dogovor.pdf'
-    # basedir = os.path.abspath(os.getcwd())
-    basedir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    filepath = '/static/files/dogovor.pdf'
 
-    try:
-        filepath = os.path.abspath(os.path.join(basedir, dogovor_path))
-    except Exception:
-        filepath = dogovor_path
-
-    return render(request, 'dogovor/dogovor.html', {'filepath': filepath, 'basedir': basedir})
-
-    # return FileResponse(open(filepath, 'rb'), content_type='application/pdf')
+    return FileResponse(open(filepath, 'rb'), content_type='application/pdf')
