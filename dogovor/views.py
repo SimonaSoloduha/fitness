75d7bc7f6ec7eb87d78d492a4755aa3f1dd3edf3
simonasoloduha <ls460simona@gmail.com>
2025-01-23 16:43:09 +0300

    fix path dogovor

--- dogovor/views.py
+++ dogovor/views.py
@@ -4,6 +4,6 @@ import os
 
 
 def dogovor(request):
-    filepath = '/static/files/dogovor.pdf'
+    filepath = 'static/files/dogovor.pdf'
 
     return FileResponse(open(filepath, 'rb'), content_type='application/pdf')
