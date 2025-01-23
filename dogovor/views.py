3ef92bd7d27ad2568e78d8c90546053b82a5d1ed
simonasoloduha <ls460simona@gmail.com>
2025-01-23 17:02:02 +0300

    fix path dogovor

--- dogovor/views.py
+++ dogovor/views.py
@@ -4,12 +4,6 @@ import os
 
 
 def dogovor(request):
-    dogovor_path = 'static/files/dogovor.pdf'
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    try:
-        file_path = os.path.join(current_dir, dogovor_path)
-    except Exception:
-        file_path = current_dir
-    return render(request, 'dogovor/dogovor.html', {'file_path': file_path})
 
-    # return FileResponse(open(file_path, 'rb'), content_type='application/pdf')
+    return render(request, 'dogovor/dogovor.html', {'file_path': current_dir})
