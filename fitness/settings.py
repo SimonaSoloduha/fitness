977bb1b3b7007ad71d285cc9678230adf5f94165
simonasoloduha <ls460simona@gmail.com>
2026-08-15 18:19:04 +0300

    add pay in yandex

--- fitness/settings.py
+++ fitness/settings.py
@@ -184,7 +184,8 @@ STATICFILES_DIRS = (
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 
